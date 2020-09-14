a96c376d3ab30a73285395a21d9bb5dc5edbbec3
tokmanov <71058824+tokmanov@users.noreply.github.com>
2020-09-14 11:38:05 +0300

    14.09.2020
    Привязала другой атрибут, для кнопки найти.

--- bot_for_yandex/script.js
+++ bot_for_yandex/script.js
@@ -10,15 +10,13 @@
 
 let keywords = ['Гобой', 'Как звучит флейта', 'Кларнет'];
 let keyword = keywords[getRandom(0,keywords.length)];
-let button = document.querySelectorAll('[data-bem*="button"]')[1];
+let button = document.querySelectorAll("button")[0];
 
 if (button != undefined){
-    document.getElementById('text').value = keyword;
-    document.querySelectorAll('[data-bem*="button"]')[1].click();
-
+    document.getElementById("text").value = keyword;
+    document.querySelectorAll("button")[0].click();
 }else{
     let links = document.links;
-
     for (let i=0; i<links.length; i++){
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
             console.log("Ссылка найдена "+links[i]);
