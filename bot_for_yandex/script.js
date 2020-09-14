c7bd4916c6462a30156810d94481756671265085
tokmanov <71058824+tokmanov@users.noreply.github.com>
2020-09-14 14:02:03 +0300

    14.09.2020
    1. Ключевое слово набирается по буквам.
    2. Сделала задержку по времени между действиями скрипта.
    3. Научили скрипт работать на искомом сайте.
    4. Скрипт умеет искать сайт на разных страницах поисковика до 7 страницы.
    5. Скрипт переходит по случайным ссылкам на искомом сайте.
    6. С вероятностью в 30 % процентов скрипт возвращается на сайт поисковика яндекс.

--- bot_for_yandex/script.js
+++ bot_for_yandex/script.js
@@ -5,26 +5,54 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
+// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
 // @grant        none
 // ==/UserScript==
 
-let keywords = ['Гобой', 'Как звучит флейта', 'Кларнет'];
+let yandexInput = document.getElementById('text');
+let keywords = ['Гобой', 'Как звучит флейта', 'Кларнет', 'Саксофон'];
 let keyword = keywords[getRandom(0,keywords.length)];
 let button = document.querySelectorAll(".button_theme_websearch")[0];
-
+let i = 0;
+let links = document.links;
 
 if (button != undefined){
-    document.getElementById("text").value = keyword;
-    document.querySelectorAll(".button_theme_websearch")[0].click();
+    let timerId = setInterval(function(){
+        yandexInput.value += keyword[i];
+        i++
+    if (i==keyword.length){
+        clearInterval(timerId);
+        button.click();
+    }
+   },1000);
+}else if(location.hostname == "xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
+    setInterval(()=>{
+        let index = getRandom(0,links.length);
+        if (getRandom(0,101)>=70){
+        location.href = 'https://yandex.ru/';
+
+}else if (links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
+    links[index].click();
+}},getRandom(3000,7000));
+
 }else{
-    let links = document.links;
-    for (let i=0; i<links.length; i++){
+    let nextYandexPage = true;
+    for(let i=0; i<links.length; i++){
+
         if(links[i].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai") != -1){
-            console.log("Ссылка найдена "+links[i]);
+            let link = links[i];
+            nextYandexPage = false;
             links[i].removeAttribute('target');
-            links[i].click();
+            setTimeout(()=>{link.click();},getRandom(1000,4000));
             break;
         }
+    }
+    if (document.querySelector('[aria-label*="Текущая страница 7"]')){
+        nextYandexPage = false;
+        location.href = 'https://yandex.ru/';
+    }
+    if (nextYandexPage){
+       setTimeout(()=>{document.querySelector('[aria-label*="Следующая страница"]').click();},getRandom(1000,4000));
     }
 }
 function getRandom(min,max){
